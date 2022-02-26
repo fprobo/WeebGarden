8371355d5a900e13fb6fbc92c899ee173a475bfb
fprobo <federico.probo@gmail.com>
2022-02-26 12:18:26 +0100

    echarts added

--- weebGarden/src/app/app.component.ts
+++ weebGarden/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EChartsOption } from 'echarts';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'weebGarden';
+  value = '';
+
+  changeValue() {
+    this.value = "Prova"
+  }
+
+
+chartOption: EChartsOption = {
+  xAxis: {
+    type: 'category',
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+  },
+  yAxis: {
+    type: 'value',
+  },
+  series: [
+    {
+      data: [820, 932, 901, 934, 1290, 1330, 1320],
+      type: 'line',
+    },
+  ],
+};
 }
