cf0acdd0a61a4703e954482c58a2f47c8e99d3e6
fprobo <federico.probo@gmail.com>
2022-04-16 11:27:18 +0200

    clean textArea and nav added

--- weebGarden/src/app/app.component.ts
+++ weebGarden/src/app/app.component.ts
@@ -13,15 +13,33 @@ export class AppComponent {
   data: any;
   jsonInput: string = "";
   formattedData: any;
+  tabSelected: number = 1;
+
+  chartOption: EChartsOption = {
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line',
+      },
+    ],
+  };
 
   constructor() { }
 
   toJSON() {
-    console.log("JSON: ", JSON.parse(this.jsonInput))
-    let data = JSON.parse(this.jsonInput);
-    console.log("JSON: ", data.filter((x: any) => x.error == undefined))
-    this.formattedData = data.filter((x: any) => x.error == undefined)
+    console.log("JSON: ", JSON.parse(this.jsonInput).filter((x: any) => x.error == undefined));
+    this.tabSelected = 1;
+    this.formattedData = JSON.parse(this.jsonInput)?.filter((x: any) => x.error == undefined).map((x: any) => x.data);
   }
 
-  getName = (manga: any) => { return manga.title_english ? manga.title_english : manga.title_japanese}
+  cleanTextArea = () => { this.jsonInput = ""; this.formattedData = [] }
+
+  getName = (manga: any) => { return manga.title_english ? manga.title_english : manga.title_japanese }
 }
