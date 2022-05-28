506ce9f35a9f85148b13d8ba90fc40b8d1c1e760
fprobo <federico.probo@gmail.com>
2022-05-28 19:32:59 +0200

    statistiche page wip

--- weebGarden/src/app/app.component.ts
+++ weebGarden/src/app/app.component.ts
@@ -11,9 +11,13 @@ export class AppComponent {
   value = '';
   data: any;
   jsonInput: string = "";
-  formattedData: any;
+  formattedData: Array<any> = [];
   tabSelected: number = 1;
   selectedManga: any;
+  groupByAuthor: Array<any> = [];
+  dataByScore: Array<any> = [];
+  dataByLength: Array<any> = [];
+  dataByPublishDate: Array<any> = [];;
 
   chartOption: EChartsOption = {
     xAxis: {
@@ -34,22 +38,58 @@ export class AppComponent {
   constructor(private modalService: NgbModal) { }
 
   toJSON() {
-    console.log("JSON: ", JSON.parse(this.jsonInput).filter((x: any) => x.error == undefined));
     this.tabSelected = 1;
     this.formattedData = JSON.parse(this.jsonInput)?.filter((x: any) => x.error == undefined).map((x: any) => x.data);
+    console.log("JSON: ", this.formattedData);
+    this.groupByMangaByAuthor();
+    this.dataByLength = this.sortByLength();
+    this.dataByScore = this.sortByScore();
+    this.dataByPublishDate = this.sortByPublishDate()
   }
 
   cleanTextArea = () => { this.jsonInput = ""; this.formattedData = [] }
 
-  getName = (manga: any) => { return manga.title_english ? manga.title_english : manga.title_japanese }
+  getName = (manga: any) => { return manga.title_english ? manga.title_english : manga.title }
+
+  formatMangakaName(name: string){
+     let nameSplitted = name.split(',');
+     return nameSplitted[1] + " " + nameSplitted[0];
+  }
   
   open(content: any, id: number) {
     this.selectedManga = this.formattedData.find((x: any) => x.mal_id == id);
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'});
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title' });
   }
 
   closeModal() {
     this.modalService.dismissAll();
     this.selectedManga = undefined;
   }
+
+  groupByMangaByAuthor() {
+    this.groupByAuthor = Object.values(this.groupBy(this.formattedData, x => x.authors[0].mal_id)).sort().reverse();
+  }
+
+  sortByLength() {
+    let tempData = [ ...this.formattedData.filter(x => x.chapters != null)];
+    return tempData.sort((a: any, b:any) => b.chapters - a.chapters);
+  }
+
+  sortByPublishDate() {
+    let tempData = [ ...this.formattedData.filter(x => x.chapters != null)];
+    return tempData.sort((a: any, b:any) => new Date(a.published.from).getTime() - new Date(b.published.from).getTime());
+  }
+
+  sortByScore() {
+    let tempData = [ ...this.formattedData];
+    return tempData.sort((a: any, b:any) => b.score - a.score);
+  }
+  
+  groupBy = <T, K extends keyof any>(list: T[], getKey: (item: T) => K) =>
+  list.reduce((previous, currentItem) => {
+    const group = getKey(currentItem);
+    if (!previous[group]) previous[group] = [];
+    previous[group].push(currentItem);
+    return previous;
+  }, {} as Record<K, T[]>);
 }
